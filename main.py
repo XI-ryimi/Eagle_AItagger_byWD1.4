5da39a49591a9fa3692ba73dd06149a06c865c03
欲叩ykso <1595472436@qq.com>
2024-08-09 21:14:59 +0800

    debug

--- main.py
+++ main.py
@@ -14,67 +14,71 @@ def TagsToJson(file) -> None:
     print(f'正在处理文件: {img_working}')
 
     # 读取txt，从tags中删除不必要的标签
-    re_tags_ToDel_1 = r'(?:\d|\d_|multiple|multiple_)(?:girl|boy|girls|boys)'
-    re_tags_ToDel_2 = r'\b\w+_(?:quality|background)\b'
-    re_tags_ToDel_3 = r'solo|masterpiece|illustration'
-    re_tags_ToDel_4 = r'^[\s,]+|[\s,]+$'
+    if txt_working is not None:
+        re_tags_ToDel_1 = r'(?:\d|\d_|multiple|multiple_)(?:girl|boy|girls|boys)'
+        re_tags_ToDel_2 = r'\b\w+_(?:quality|background)\b'
+        re_tags_ToDel_3 = r'solo|masterpiece|illustration'
+        re_tags_ToDel_4 = r'^[\s,]+|[\s,]+$'
 
-    try:
-        with txt_working.open(mode = 'r', encoding='utf-8') as f:
-            txt_OriTags = f.read()
-            txt_CleanedTags = re.sub(re_tags_ToDel_1, '', txt_OriTags)
-            txt_CleanedTags = re.sub(re_tags_ToDel_2, '', txt_CleanedTags)
-            txt_CleanedTags = re.sub(re_tags_ToDel_3, '', txt_CleanedTags)
-            txt_CleanedTags = txt_CleanedTags.replace(' ,', '')
-            txt_CleanedTags = re.sub(re_tags_ToDel_4, '', txt_CleanedTags)
+        try:
+            with txt_working.open(mode = 'r', encoding='utf-8') as f:
+                txt_OriTags = f.read()
+                txt_CleanedTags = re.sub(re_tags_ToDel_1, '', txt_OriTags)
+                txt_CleanedTags = re.sub(re_tags_ToDel_2, '', txt_CleanedTags)
+                txt_CleanedTags = re.sub(re_tags_ToDel_3, '', txt_CleanedTags)
+                txt_CleanedTags = txt_CleanedTags.replace(' ,', '')
+                txt_CleanedTags = re.sub(re_tags_ToDel_4, '', txt_CleanedTags)
 
-            txt_Tags_list = [
-                tag.strip() 
-                for tag in txt_CleanedTags.split(', ') 
-                if tag.strip()
-            ]
-
-            # 汉化tags
-            if TransIf is False:
-                txt_CNTags = txt_Tags_list
-            else:
-                txt_CNTags = [
-                    TransDic_data.get(tag, tag)
-                    for tag in txt_Tags_list 
+                txt_Tags_list = [
+                    tag.strip() 
+                    for tag in txt_CleanedTags.split(', ') 
+                    if tag.strip()
                 ]
-    except Exception as e:
-        print(f'txt文件{txt_working}读取失败: {e}')
-        txt_except_list.append(txt_working)
-    
-    # tag写入json
-    try:
-        with json_working.open(mode = 'r', encoding='utf-8') as f:
-            json_working_data = json.load(f)
-            json_working_tags = json_working_data.get("tags",[])
-            new_tags = [
-                tag for tag in txt_CNTags 
-                if tag not in json_working_tags
-            ]
-            if new_tags:
-                json_working_tags.extend(new_tags)
-                json_working_data['tags'] = json_working_tags
-                with open(json_working, 'w', encoding='utf-8') as f:
-                    json.dump(json_working_data, f, ensure_ascii=False)
-            else:
-                print(f'{json_working}没有新tag需要写入')
-    except json.JSONDecodeError as e:
-        print(f'json文件{json_working}解析错误: {e}')
-        json_except_list.append(json_working)
 
-# 多线程 
+                # 汉化tags
+                if TransIf is False:
+                    txt_CNTags = txt_Tags_list
+                else:
+                    txt_CNTags = [
+                        TransDic_data.get(tag, tag)
+                        for tag in txt_Tags_list 
+                    ]
+        except Exception as e:
+            print(f'txt文件{txt_working}读取失败: {e}')
+            except_list.append(img_working)
+        # tag写入json
+        if json_working is not None:
+            try:
+                with json_working.open(mode = 'r', encoding='utf-8') as f:
+                    json_working_data = json.load(f)
+                    json_working_tags = json_working_data.get("tags",[])
+                    new_tags = [
+                        tag for tag in txt_CNTags 
+                        if tag not in json_working_tags
+                    ]
+                    if new_tags:
+                        json_working_tags.extend(new_tags)
+                        json_working_data['tags'] = json_working_tags
+                        with open(json_working, 'w', encoding='utf-8') as f:
+                            json.dump(json_working_data, f, ensure_ascii=False)
+                    else:
+                        print(f'{json_working}没有新tag需要写入')
+            except json.JSONDecodeError as e:
+                print(f'json文件{json_working}解析错误: {e}')
+                except_list.append(img_working)
+        else:
+            print(f'json文件{json_working}不存在')
+            except_list.append(img_working)
+    else:
+        print(f'{img_working}不存在txt')
+        except_list.append(img_working)
 
 # 工作目录
 print('每次处理不建议超过1W张，根据你的电脑性能自行决定要处理的图片数量')
 print('AI标记tags的前处理是否已完成？')
 print(f'粘贴从[Eagle]获取的图片路径\n{"-"*50}')
 
-txt_except_list = []
-json_except_list = []
+except_list = []
 TransDic_data = {}
 combined_list = []
 use_multithread: bool
@@ -95,7 +99,7 @@ while True:
     work_dir = work_dir.group(1)
 
     if work_dir.endswith('.library'):
-        print(f'\n{"-"*50}Eagle资源库路径：{work_dir}\n{"-"*50}')
+        print(f'{"-"*50}\nEagle资源库路径：{work_dir}\n{"-"*50}')
         break
 
     print('路径异常：', work_dir)
@@ -105,27 +109,20 @@ img_list = [
     for p in img_input_list.split(" ")
 ]
 
-# 获取父目录
-# 获取文件名
-# 拼接txt与json路径
-# 异常路径
-# 更新imglist
-txt_list = []
+txt_list = [
+    path.with_suffix('.txt') 
+    if path.with_suffix('.txt').exists() else None
+    for path in img_list
+]
 
-json_list = []
-
-txt_except_list = []
-json_except_list = []
+json_list = [
+    path.parent / 'metadata.json' 
+    if (path.parent / 'metadata.json').exists() else None
+    for path in img_list
+]
 
 combined_list = list(zip(img_list, txt_list, json_list))
 
-print(f'[调试：],txt_list:{txt_list}\n{"-"*50}')
-print(f'[调试：],txt_except_list:{txt_except_list}\n{"-"*50}')
-print(f'[调试：],combined_list:{combined_list}\n{"-"*50}')
-
-if not combined_list:
-    input('没有要处理的图片或txt全部异常')
-    sys.exit(0)
 print(f'待处理{len(combined_list)}个图片\n{"-"*50}')
 
 # 汉化tags
@@ -180,15 +177,17 @@ else:
         # 使用线程池提交任务
         results = [executor.submit(TagsToJson, file) for file in combined_list]
         concurrent.futures.wait(results)
-if combined_list:
-    if txt_except_list or json_except_list:
-        except_list = txt_except_list + json_except_list
-        print(f'{len(except_list)}个异常文件：\n{except_list}')
-    print(f'文件处理完成\n{"-"*50}')
-# # 删除所有txt文件
-# for txt_file in txt_list:
-#     try:
-#         os.remove(txt_file)
-#         print(f'已删除文件: {txt_file}')
-#     except Exception as e:
-#         print(f'删除文件失败 {txt_file}: {e}')
+
+if except_list:
+    except_list = set(except_list)
+    print(f'{"-"*50}\n{len(except_list)}个异常文件：\n{except_list}')
+    
+print(f'{"-"*50}\n文件处理完成\n{"-"*50}')
+
+# 删除所有txt文件
+for txt_file in txt_list:
+    try:
+        os.remove(txt_file)
+        print(f'已删除文件: {txt_file}')
+    except Exception as e:
+        print(f'删除文件失败 {txt_file}: {e}')
